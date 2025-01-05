7a954d0c458670d638f442d618b8ba3b6963e692
MineHighVN <htag919@gmail.com>
2025-01-05 14:32:31 +0700

    update template file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 const { _holo, holo } = require("@holo/dev");
 
+const HelloWorld = ({ children }) => {
+    return _holo("p", {
+        children: `Hello world from ${children}`,
+    });
+};
+
+const TestComponent = () => {
+    return _holo("Fragment", {
+        children: [
+            _holo("p", {
+                children: `Hello world from @holo/dev`,
+            }),
+            _holo("p", {
+                children: "Author: MineHighVN",
+            }),
+            _holo(HelloWorld, {
+                children: "MineHighVN",
+            }),
+        ],
+    });
+};
+
 const App = () => {
     holo.window.setTitle("Holo Project (@holo/dev)");
 
-    return _holo("p", {
-        children: `Hello world from @holo/dev`,
+    return _holo("div", {
+        children: _holo(TestComponent),
     });
 };
 
