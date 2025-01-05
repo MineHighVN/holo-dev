ed53cd6b33b29e90c31e8bec9bc90ac46938bc8f
MineHighVN <htag919@gmail.com>
2025-01-05 14:30:35 +0700

    allow reuse components

--- packages/HoloCore/index.js
+++ packages/HoloCore/index.js
@@ -9,11 +9,11 @@ const init = require("./src/init");
  *
  * @property {Object} hpc - All method about HPC
  * @property {(a: string, b: any) => void} hpc.registerHPC - Register HPC for handle event
- * @property {(nodeName: string) => number} holo.createNode - Create new node
  *
  * @property {Object} document - Control everything on view - C++
  * @property {(NodeID: number, children: (string | number)) => void} document.setChildren - set children for a node
  * @property {(NodeID: number, children: (string | number)) => void} document.appendChildren - append a node child to another node
+ * @property {(nodeName: string) => number} document.createNode - Create new node
  *
  */
 
